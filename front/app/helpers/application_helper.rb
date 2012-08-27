2f67191812581709c7f9c87087480843a48f2def
Felipe Espinoza <fespinozacast@gmail.com>
2012-08-27 18:27:50 -0400

    refactored modal confirmation links

--- front/app/helpers/application_helper.rb
+++ front/app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def confirmation_delete_link(object, url, modal_title, modal_content)
+    modal_id = "delete_#{object.class.to_s.downcase}_#{object.id}"
+    render 'delete_confirmation', object: object, url: url, modal_id: modal_id, modal_title: modal_title, modal_content: modal_content
+  end
 end
