d361cc6518fe4972c13699b55dd0803a07aeed32
Felipe Espinoza <fespinozacast@gmail.com>
2012-08-27 17:59:40 -0400

    delete all user's accounts when destroyed

--- front/spec/models/user_spec.rb
+++ front/spec/models/user_spec.rb
@@ -13,7 +13,19 @@ describe User do
 
     it "returns the new account" do
       subject.new_account('wallet', -2000).should be_kind_of(Account)
-
+    end
+  end
+  
+  describe "#destroy" do
+    subject { create(:user) }
+    it "removes the user" do
+      subject.destroy
+      expect { User.find(subject.id) }.to raise_error(ActiveRecord::RecordNotFound)
+    end
+    it "removes all his accounts" do
+      3.times { create(:account, user: subject) }
+      subject.destroy
+      Account.where(user_id: subject.id).should be_empty
     end
   end
 end
